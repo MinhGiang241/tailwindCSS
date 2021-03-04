9e8170bcc317f02ef4701368b91501ce5d6352ec
MinhGiang241 <minhgiang241@gmail.com>
2021-03-04 13:11:12 +0700

    demo tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+const tailwind = require("tailwindcss/plugin");
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    tailwind(function ({ addComponents }) {
+      const buttons = {
+        ".btn-red": {
+          padding: ".5rem 1rem",
+          borderRadius: ".25rem",
+          fontWeight: "600",
+          backgroundColor: "#e3342f",
+          color: "#fff",
+          "&:hover": {
+            backgroundColor: "#cc1f1a",
+          },
+        },
+      };
+
+      addComponents(buttons);
+    }),
+  ],
+};
